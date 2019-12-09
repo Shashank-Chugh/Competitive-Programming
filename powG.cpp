790ac29cc6238f558d3bbdc77abccd1734cf4662
Shashank-Chugh <42238022+Shashank-Chugh@users.noreply.github.com>
2019-12-09 20:17:57 +0530

    Create powG.cpp

---
+++ powG.cpp
@@ -0,0 +1,19 @@
+ll fun(ll x,ll y)
+{
+  return x*y;
+}
+ll powG(ll x,ll y )
+{
+
+  if(y==1)return x;
+  if(y==0)return 1;
+
+  ll temp=powG(x,y/2);
+
+  temp = fun(temp,temp);
+
+  if(y&1)
+    temp=fun(temp,x);
+  
+  return temp;
+}
