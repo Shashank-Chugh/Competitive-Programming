0ed43da5c3dd998a7c6aa23ea45730e01420b57c
Shashank-Chugh <42238022+Shashank-Chugh@users.noreply.github.com>
2019-12-10 20:34:14 +0530

    Update powMod.cpp

--- powMod.cpp
+++ powMod.cpp
@@ -1,12 +1,31 @@
+ll modmul(ll a, ll b, ll c)
+{   
+    a%=c;
+
+    ll ans = 0;
+
+    while(b)
+    {
+        if(b&1)
+        {
+            ans = (ans+a)%c;
+        }
+
+        a = (a+a)%c;
+        b >>= 1;
+    }
+
+    return ans;
+}
 ll powMod(ll n,ll p,ll m)
 {
     ll res=1;
     n%=m;
     while(p){
         if(p&1)
-            res = (res*n)%m;
+            res = modmul(res,n,m)%m;
         p=p>>1;
-        n = (n*n)%m;
+        n = modmul(n,n,m)%m;
     }
     return res;
 }
