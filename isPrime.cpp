2f957019e1975d0e3ce5a05e51dfcac9bd3a4a8d
Shashank-Chugh <42238022+Shashank-Chugh@users.noreply.github.com>
2019-12-11 13:58:32 +0530

    Create isPrime.cpp

---
+++ isPrime.cpp
@@ -0,0 +1,15 @@
+bool isPrime(ll n){
+    if(n<=1)
+        return false;
+    if(n==2)
+        return true;
+    if((n&1)==0)
+        return false;
+        
+    ll rt = sqrt(n);
+    for(ll i=3;i<=rt;i+=2){
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
