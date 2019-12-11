65df1f2d0cb905ec8ae9774bab10e16a1b94b7d7
Shashank-Chugh <42238022+Shashank-Chugh@users.noreply.github.com>
2019-12-11 12:48:32 +0530

    Create sieve.cpp

---
+++ sieve.cpp
@@ -0,0 +1,50 @@
+void sieve1(ll*a,ll n) //to get if a no. is prime or not in array
+{
+   fill(a,a+n,1);
+   a[0]=a[1]=0;
+   ll i;
+   rep(i,2,n)
+   {
+      if(a[i]>1)
+         continue;
+      ll j=i*2;       // j=i*i if you dont want composite no. to get checked by all its prime factors
+      while(j<n)
+      {
+         a[j]++;
+         j+=i;
+      }
+   }
+
+   rep(i,2,n)
+      if(a[i]!=1)
+         a[i]--;
+
+} 
+
+
+
+
+
+
+vector<ll> sieve2(ll *a,ll n) //to get least prime that divides this no. in array 
+{
+    vector<ll int>v;
+    fill(a,a+n,0);
+    ll i;
+    rep(i,2,n)
+    {
+        if(a[i]>0)
+            continue;
+        a[i] = i;
+        ll j=i*i;
+        while(j<n)
+        {
+            if(a[j]==0)
+                a[j]=i;
+            j+=i;
+        }
+        v.pb(i); 
+    }
+    return v;
+}
+   
