1ef6e8fb7ac394b3c1f0ef18fc9d9751d66c4b06
Shashank-Chugh <42238022+Shashank-Chugh@users.noreply.github.com>
2019-12-30 12:46:36 +0530

    unimodal extremas

--- ternarySearch.cpp
+++ ternarySearch.cpp
@@ -1,6 +1,6 @@
 double func(double x)
 { 
-return -1*1*x*x + 2*x +3; 
+return -1*x*x + 2*x +3; 
 }
 
 // greater than log2(p) / log3(p) 
@@ -13,7 +13,7 @@ double ts(double start, double end)
       double l1 = (l*2+r)/3;
       double l2 = (l+2*r)/3;
       //cout<<l1<<" "<<l2<<endl;
-      if(func(l1) > func(l2)) r = l2; else l = l1;
+      if(func(l1) > func(l2)) r = l2; else l = l1; //for maxima ,if minima change > to <
       }
 
      double x = l;
