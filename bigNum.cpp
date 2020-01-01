db63284cf9313dfee6a7934c2ecdf2c9eb7504f8
Shashank-Chugh <42238022+Shashank-Chugh@users.noreply.github.com>
2020-01-01 21:14:02 +0530

    Create bigNum.cpp

---
+++ bigNum.cpp
@@ -0,0 +1,62 @@
+string summ(string str1, string str2) 
+{ 
+    // Before proceeding further, make sure length 
+    // of str2 is larger. 
+    if (str1.length() > str2.length()) 
+        swap(str1, str2); 
+  
+    // Take an empty string for storing result 
+    string str = ""; 
+  
+    // Calculate length of both string 
+    int n1 = str1.length(), n2 = str2.length(); 
+  
+    // Reverse both of strings 
+    reverse(str1.begin(), str1.end()); 
+    reverse(str2.begin(), str2.end()); 
+  
+    int carry = 0; 
+    for (int i=0; i<n1; i++) 
+    { 
+        // Do school mathematics, compute sum of 
+        // current digits and carry 
+        int sum = ((str1[i]-'0')+(str2[i]-'0')+carry); 
+        str.push_back(sum%10 + '0'); 
+  
+        // Calculate carry for next step 
+        carry = sum/10; 
+    } 
+  
+    // Add remaining digits of larger number 
+    for (int i=n1; i<n2; i++) 
+    { 
+        int sum = ((str2[i]-'0')+carry); 
+        str.push_back(sum%10 + '0'); 
+        carry = sum/10; 
+    } 
+  
+    // Add remaining carry 
+    if (carry) 
+        str.push_back(carry+'0'); 
+  
+    // reverse resultant string 
+    reverse(str.begin(), str.end()); 
+  
+    return str; 
+} 
+string compare(string s1,string s2)
+{
+    if(s1.size()>s2.size())
+        return s2;
+    else if(s2.size()>s1.size())
+        return s1;
+    else
+    {
+        int i;
+        rep(i,0,s2.size())
+        {
+            if(s1[i]>s2[i])return s2;
+            else if(s2[i]>s1[i])return s1;
+        }
+    }     
+}
