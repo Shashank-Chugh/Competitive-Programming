eabaa03dc076b16034ba149942f6d4b6fa778ad2
Shashank-Chugh <42238022+Shashank-Chugh@users.noreply.github.com>
2020-01-02 14:15:05 +0530

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,99 +1,38 @@
-class graph
+void bfs(vector<int>*a,int*visited,int n,int sv,queue<int>q)
 {
-    public: 
-        int n_v,n_e;
-        vector<vector<int>> adj_list;
-        bool isDirected;
-        vector<int> isVisited;
-        vector<int> indeg; //Stores indegree of all Nodes
-        vector<int> topo; //Stores lexicographically smallest toposort
-
-        void take_input(bool =true);
-
-        void show_adj_list();
-
-        void addEdge(int ,int ,bool =true);
-
-        void dfs(int );
-
-        void bfs(int );
-
-        bool toposort();
-
-};
-
-void graph::take_input(bool isDir)
-{
-    cin>>n_v>>n_e;
-    adj_list.resize(n_v);
-    isVisited.resize(n_v);
-    fill(all(isVisited),-1);
-    isDirected = isDir;
-    int to,fro;
-    for(int i=0;i<n_e;++i)
-    {
-        cin>>to>>fro;
-        addEdge(to-1,fro-1,isDir);
-    }
-}
-
-void graph::show_adj_list()
-{
+    cout<<sv<<" ";
+    visited[sv]=1;
     int i;
-    cout<<"No. of Vertices = "<<n_v<<en;
-    cout<<"No. of Edges = "<<n_e<<en;
-    rep(i,0,n_v)
-    {
-        int j;
-        cout<<i<<"-> ";
-        rep(j,0,adj_list[i].size())
-        {
-            cout<<adj_list[i][j]<<" ";
-        }
-        cout<<en;
-    }
-}
 
-void graph::addEdge(int to,int fro,bool isDir)
-{
-    adj_list[to].push_back(fro);
-    if(isDir)
-        adj_list[fro].push_back(to);
-}
-void graph::bfs(int source)
-{
-    fill(all(isVisited),-1);
-    queue<int> q;
-    q.push(source);
-    isVisited[source] = 0;
-    while(!q.empty())
+    for(i=0;i<a[sv].size();i++)
     {
-        int parent = q.front();
-        cout<<parent+1<<" ";
+        if(visited[a[sv][i]]==0)
+        {
+            visited[a[sv][i]]=1;
+            q.push(a[sv][i]);
+        }
+            
+    }
+    
+    if(q.size()!=0)
+    {
+        int s=q.front();
         q.pop();
-        int child;
-        rep(child,0,adj_list[parent].size())
-        {
-            if(isVisited[adj_list[parent][child]]==-1)
-            {
-                q.push(adj_list[parent][child]);
-                isVisited[adj_list[parent][child]] = isVisited[parent] + 1;
-            }
-        }
-    }
-}
-void graph::dfs(int source)
-{
-    isVisited[source] = 1;
-    cout<<source+1<<" "; 
-    int i;
-    rep(i,0,adj_list[source].size())
-        if(isVisited[adj_list[source][i]]==-1)
-            dfs(adj_list[source][i]);
-}
+    
+      bfs(a,visited,n,s,q);
 
-/*Use Case
-  graph g;
-    g.take_input();
-    g.show_adj_list();
-*/
+    }
+
+}
+void dfs(vector<int>*a,int*visited,int n,int sv)
+{
+    cout<<sv<<" ";
+    visited[sv]=1;
+    int i;
+
+    for(i=0;i<a[sv].size();i++)
+    {   
+        if(visited[a[sv][i]]==0)
+            dfs(a,visited,n,a[sv][i]);                    
+    }
+}    
