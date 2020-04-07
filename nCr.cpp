b82e16de21112280190e943cf6ff61b0e1e4f727
abcd <42238022+Shashank-Chugh@users.noreply.github.com>
2020-04-07 12:13:29 +0530

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -1,52 +1,63 @@
 ll gcdExtended(ll a,ll b,ll &x,ll &y)
 {
-	// Base Case
-	if (a == 0)
-	{
-		x = 0, y = 1;
-		return b;
-	}
+    // Base Case
+    if (a == 0)
+    {
+        x = 0, y = 1;
+        return b;
+    }
 
-	ll x1, y1; // To store results of recursive call
-	ll gcd = gcdExtended(b%a, a, x1, y1);
+    ll x1, y1; // To store results of recursive call
+    ll gcd = gcdExtended(b%a, a, x1, y1);
 
-	// Update x and y using results of recursive
-	// call
-	x = y1 - (b/a) * x1;
-	y = x1;
+    // Update x and y using results of recursive
+    // call
+    x = y1 - (b/a) * x1;
+    y = x1;
 
-	return gcd;
+    return gcd;
 }
 
 ll modInverse(ll a,ll m)
 {
-	ll x, y;
-	ll g = gcdExtended(a, m, x, y);
-	if (g != 1)
-		cout << "Inverse doesn't exist";
-	else
-	{
-		// m is added to handle negative x
-		ll res = (x%m + m) % m;
+    ll x, y;
+    ll g = gcdExtended(a, m, x, y);
+    if (g != 1)
+        cout << "Inverse doesn't exist";
+    else
+    {
+        // m is added to handle negative x
+        ll res = (x%m + m) % m;
         return res;
-	}
+    }
 }
 
-//0c0 check
-ll nCr(ll n,ll r) 
+int fact[N], invfact[N];
+
+void precompute(int m)
+{
+    fact[0]=fact[1]=1;
+    for(int i=2;i<N;i++)
+    {
+        fact[i]=(fact[i-1]*i)%m;
+    }
+    invfact[N-1]=modInverse(fact[N-1] ,m);
+    for(int i=N-2;i>=0;i--)
+    {
+        invfact[i]=(invfact[i+1]*(i+1))%m;
+    }
+}
+
+ll nCr(ll n,ll r,int m) 
 { 
    // Base case  
-  	if(n==0||n<r)
-  		return 0;
-  	if (r==0) 
-    	return 1;	
+    if(n==0||n<r)
+        return 0;
+    if (r==0||n==r) 
+        return 1;   
   
     // Fill factorial array so that we can find all factorial of r, n and n-r 
-    ll fac[n+1]; 
-    fac[0] = 1; 
-    for (ll i=1 ; i<=n; i++){ 
-        fac[i] = fac[i-1]*(i%mod);
-        fac[i] %= mod;
-    }
-    return (fac[n]*modInverse(fac[r],mod)%mod * modInverse(fac[n-r],mod))%mod; 
+    
+    return ( ((fact[n]*invfact[r]) %m) * invfact[n-r] )%m; 
 }
+
