529bad3edbdb394613f564080be40a388f8fc597
abcd <42238022+Shashank-Chugh@users.noreply.github.com>
2020-05-13 11:01:31 +0530

    Update calculateFactor.cpp

--- calculateFactor.cpp
+++ calculateFactor.cpp
@@ -1,17 +1,17 @@
-//do n^2(select any two numbers) selection in set returned feasible if n is 1e9
-set<int> calculateFactor(int n) 
+//do n^2(select any two numbers) selection in set returned feasible if n is 1e9 ( sqrt(n) factors )
+//CHECK  (duplicates | sort ? )
+vector<int> calculateFactor(int n) 
 { 
     // Note that this loop runs till square root 
-  set<int> temp;
+  vector<int> temp;
   int l = sqrt(n);
     for (int i=1; i<=l; i++) 
     { 
         if (n%i == 0) 
         { 
-            temp.insert(i);
-            temp.insert(n/i);
+            temp.pb(i);
+            temp.pb(n/i);
         }
     } 
     return temp;
 } 
-
